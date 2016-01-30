7eba0eb8deede7dad8d107fb253fef49499e8190
Silly511 <remy555@icloud.com>
2016-01-30 12:22:48 -0600

    Added Sources Code

---
+++ src/main/java/net/sparklepopprograms/core/DimensionalCore.java
@@ -0,0 +1,50 @@
+package net.sparklepopprograms.core;
+
+import scala.Int;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLInterModComms;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import cpw.mods.fml.common.Mod.EventHandler;
+
+
+@Mod(modid = DimensionalCore.modid, name = "DimensionalCore", version = DimensionalCore.version)
+
+public class DimensionalCore {
+	
+	public static final String modid = "DimensionalCore";
+	public static final String version = "1.0.0";
+
+	@EventHandler
+	public void load(FMLPreInitializationEvent event) {
+		
+		
+	}
+	
+	@EventHandler
+	public void load(FMLInitializationEvent event) {
+		
+		
+		
+	}
+	
+	@EventHandler
+	public void load(FMLPostInitializationEvent event) {
+		
+		
+		
+	}
+}
