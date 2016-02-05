286476c936d391310c55ac6586ae4dc94c585375
Silly511 <remy555@icloud.com>
2016-02-05 11:21:40 -0600

    Version 1.0.1 is out

--- src/main/java/net/sparklepopprograms/core/DimensionalCore.java
+++ src/main/java/net/sparklepopprograms/core/DimensionalCore.java
@@ -26,7 +26,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 public class DimensionalCore {
 	
 	public static final String modid = "DimensionalCore";
-	public static final String version = "1.0.0";
+	public static final String version = "1.0.1";
 
 	@EventHandler
 	public void load(FMLPreInitializationEvent event) {
